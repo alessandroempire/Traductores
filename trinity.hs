e61eb995afc0e8e5e9f9c08f55b33179e3465a05
Alessandro <alessandroempire@gmail.com>
2014-10-06 21:55:47 -0430

    otro arreglo de trinity

--- trinity.hs
+++ trinity.hs
@@ -1,4 +1,6 @@
 import System.Environment as SE
+import System.Exit        (exitFailure, exitSuccess)
+import Data.Sequence      as DS (null)
 import Lexer             
 
 main = do
@@ -7,3 +9,5 @@ main = do
     let (errors, tokens) = getTokens string
     print errors
     mapM_ print tokens
+    if DS.null errors then exitSuccess
+                      else exitFailure
