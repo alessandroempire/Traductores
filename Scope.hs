2da84005e2bd704f2bf1bb1ebc21acf4747d065a
alessandroempire <alessandroempire@gmail.com>
2014-10-23 17:18:03 -0430

    agregando el scope

--- Scope.hs
+++ Scope.hs
@@ -1,3 +1,29 @@
 module Scope 
     (
     ) where
+
+
+newtype Scope = Scope {serial :: ScopeNum }
+    deriving(Eq)
+
+instance Show Scope where
+    show = show . serial
+
+-- El alcane tope del programa tiene valor default 1
+topScope :: Scope
+topScope = Scope {serial = topScopeNum }
+
+-- El alcanze con todas las definiciones del lenguaje
+-- tiene valor default 0
+langScope :: Scope
+langScope = Scope {serial = langScopeNum}
+
+
+-------------------------------------------------------------
+type ScopeNum  = Int
+
+topScopeNum :: ScopeNum
+topScopeNum = 1
+
+langScopeNum :: ScopeNum
+langScopeNum = 0
